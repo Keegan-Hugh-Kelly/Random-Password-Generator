bcff557ff914e4d4b7121c5c6c6f52e891b44ab9
Keegan Hugh Kelly <kgnhughkelly@gmail.com>
2022-08-31 15:46:44 +0200

    Add files via upload

---
+++ python_app.py
@@ -0,0 +1,42 @@
+# Random Password Generator
+# By Keegan Hugh Kelly @15:18 Aug 31 2022
+
+### Whiteboard:
+
+# Ask user if they want to generate a password or not.
+# If yes, ask for password length.
+# Generate password.
+# Print password.
+# If inital response is no or invalid, exit program.
+
+import string
+import random
+
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
+def generate_password():
+    password_length = int(input("How long would you like your password to be? "))
+
+    random.shuffle(characters)
+
+    password = []
+
+    for x in range(password_length):
+        password.append(random.choice(characters))
+
+    random.shuffle(password)
+
+    password = "".join(password)
+
+    print(password)
+
+option = input("Do you want to generate a password? (Yes/No): ")
+
+if option == "Yes":
+    generate_password()
+elif option == "No":
+    print("Goodbye")
+    quit()
+else:
+    print("Invalid input, please input Yes or No")
+    quit()
